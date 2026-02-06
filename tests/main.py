bcafad857babcbca647a352b27ca322213e83396
Abhinav-EL <abhinav.yadav138@gmail.com>
2026-02-06 11:58:43 -0500

    Initial testing changes
    Needs reformatting of the eval json file.

--- tests/main.py
+++ tests/main.py
@@ -3,7 +3,7 @@ import asyncio
 
 from dotenv import load_dotenv
 import vertexai
-import test_weather_agent 
+from google.adk.evaluation.agent_evaluator import AgentEvaluator
 
 load_dotenv()
 
@@ -14,10 +14,12 @@ vertexai.init(
 )
 
 async def main():
-    """Print hello world."""
-    print("hello, starting the agent tests now!")
-    await test_weather_agent.test_agent_deployment()
+    #print("hello, starting the agent tests now!")
+    #await test_deployment_agent.test_agent_deployment()
     #await test_deployment.cleanup_deployed_agents()
+    await AgentEvaluator.migrate_eval_data_to_new_schema(
+        old_eval_data_file="tests/integration/fixture/weather_agent_eval.test.json",
+        new_eval_data_file="tests/integration/fixture/weather_agent_eval_v2.test.json")
 
 
 if __name__ == "__main__":
